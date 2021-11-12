3d04f5d1c77d5197dd5588ceee112880954f39cd
rlfah303 <rlarlfah303@gmail.com>
2021-11-12 18:00:45 +0900

    없는 숫자 더하기

---
+++ programmers/없는 숫자 더하기.py
@@ -0,0 +1,13 @@
+def solution(numbers):
+    answer = 0
+    for i in range(1,10):
+        if i not in numbers:
+            answer +=i
+    return answer
+
+"""
+
+def solution(numbers):
+    return 45 - sum(numbers)
+
+"""
