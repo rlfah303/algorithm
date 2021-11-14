3d0152416aa3d0756f0679572ee8e21708c4964e
rlfah303 <rlarlfah303@gmail.com>
2021-11-14 15:30:38 +0900

    use DFS & BFS

--- programmers/tree.py
+++ programmers/tree.py
@@ -14,6 +14,7 @@ class BinarySearchTree(object):
         self.root = self._insert_value(self.root, data)
         return self.root is not None
 
+    # Insert Method
     def _insert_value(self, node, data):
         if node is None:
             node = Node(data)
@@ -23,7 +24,8 @@ class BinarySearchTree(object):
             else:
                 node.right = self._insert_value(node.right, data)
         return node
-    
+
+    # Find Method    
     def find(self, key):
         return self._find_value(self.root, key)
 
@@ -35,6 +37,7 @@ class BinarySearchTree(object):
         else:
             return self._find_value(root.right, key)
 
+    # Delete Method
     def delete(self, key):
         self.root, deleted = self._delete_value(self.root, key)
         return deleted
@@ -66,6 +69,56 @@ class BinarySearchTree(object):
             node.right, deleted = self._delete_value(node.right, key)
         return node, deleted
 
+    # Depth First Traversal
+
+    # Pre-order traversal
+    def pre_order_traversal(self):
+        def _pre_order_traversal(root):
+            if root is None:
+                pass
+            else:
+                print(root.data)
+                _pre_order_traversal(root.left)
+                _pre_order_traversal(root.right)
+        _pre_order_traversal(self.root)
+
+    # In-order traversal
+    def in_order_traversal(self):
+        def _in_order_traversal(root):
+            if root is None:
+                pass
+            else:
+                _in_order_traversal(root.left)
+                print(root.data)
+                _in_order_traversal(root.right)
+        _in_order_traversal(self.root)
+
+    # Post-order traversal
+    def post_order_traversal(self):
+        def _post_order_traversal(root):
+            if root is None:
+                pass
+            else:
+                _post_order_traversal(root.left)
+                _post_order_traversal(root.right)
+                print(root.data)
+        _post_order_traversal(self.root)
+    # Breadth First Traversal
+
+    #Level-order traversal
+    def level_order_traversal(self):
+        def _level_order_traversal(root):
+            queue = [root]
+            while queue:
+                root = queue.pop(0)
+                if root is not None:
+                    print(root.data)
+                    if root.left:
+                        queue.append(root.left)
+                    if root.right:
+                        queue.append(root.right)
+        _level_order_traversal(self.root)
+
 #Insert
 array = [40, 4, 34, 45, 14, 55, 48, 13, 15, 49, 47]
 
@@ -76,6 +129,14 @@ for x in array:
 # Find
 print(bst.find(15)) # True
 print(bst.find(17)) # False
+
+
+# depth first
+bst.pre_order_traversal()   # 40 4 34 14 13 15 45 55 48 47 49
+bst.in_order_traversal()    # 4 13 14 15 34 40 45 47 48 49 55
+bst.post_order_traversal()  # 13 15 14 34 4 47 49 48 55 45 40
+# breadth first
+bst.level_order_traversal() # 40 4 45 34 55 14 48 13 15 47 49
 
 # Delete
 print(bst.delete(55)) # True
